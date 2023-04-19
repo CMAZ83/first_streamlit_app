9133f24f8f0061bc2a1d130409c21eec4f454b83
CMAZ83 <CMAZ83@users.noreply.github.com>
2023-04-19 20:15:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-🥣 🥗 🐔 🥑🍞
+streamlit.text('🥣 🥗 🐔 🥑🍞')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
